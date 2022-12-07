51006feb3f8a7984f19c1df4f27a73ce3d8c35a3
Romulo <romulo.sntn@gmail.com>
2022-12-07 17:35:55 -0300

    Dev commit

--- src/graph2.py
+++ src/graph2.py
@@ -3,7 +3,7 @@ class Vertex(object):
 
     id_counter = 0
 
-    def __init__(self, label, value, parent,  children_vertex, children_edges):
+    def __init__(self, label, value, parent, children_vertex, children_edges):
         self.id = Vertex.id_counter
         Vertex.id_counter += 1
 
@@ -12,6 +12,10 @@ class Vertex(object):
         self.parent = parent  # Vértice pai deste vértice
         self.children_vertex = children_vertex
         self.children_edges = children_edges
+
+    def add_edge(self, edge):
+        # self.children_edges
+        pass
 
     def __repr__(self):
         return f"{type(self).__name__}({self.id}, {self.label}, {self.parent}, {self.children_vertex}, , {self.children_edges})"
@@ -40,24 +44,29 @@ class GraphV2(object):
     vertex_counter = 0
 
     def __init__(self, matriz_adj):
+        self.root_vertex = None
         self.vertex_list = None
 
         self.add_vertex(matriz_adj)  # Adiciona os vértices.
 
-    def add_vertex(self, matriz_adj):
+    def add_vertex(self, lista_adj):
         """ Retorna a lista de vértices do grafo. """
 
-        for i in matriz_adj:
-            v = Vertex(i, 10, None, [], [])
-            print(v)
+        for i in lista_adj:
+            v1 = Vertex(i[0], 1, None, [], [])
+            v2 = Vertex(i[1], 1, v1, [], [])
+
+            e1 = Edge(v1, v2, i[2])
+
+            v1.add_edge(e1)
 
             if self.vertex_counter == 0:
-                print("Setando vertice raiz: {v}")
+                print(f'Setando vertice raiz: {v1}')
 
-                self.root_vertex = v
+                self.root_vertex = v1
 
             GraphV2.vertex_counter += 1
-    
+
     def __repr__(self):
         return f"{type(self).__name__}({self.root_vertex})"
 
@@ -65,7 +74,12 @@ class GraphV2(object):
 if __name__ == '__main__':
     print('Teste grafo')
 
-    grafo = GraphV2(['A', 'B', '15', '1a'])
+    # fileIn = 'data/in/teste-matriz-1.csv'
+    # graph_info = file_processor.read(fileIn, 'matriz')
+    graph_info = [['A', 'B', 4], ['A', 'C', 3], ['A', 'D', 2], ['A', 'E', 1]]
+
+    grafo = GraphV2(graph_info)
 
     print()
+    print(graph_info)
     print(grafo)
